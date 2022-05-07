69129fdba5c95eb8f7442d254c735a5ffe3c4f92
Jeahyun Park <jea5158@gmail.com>
2022-05-07 12:27:40 +0900

    fivonacci

--- 10870_Fibonacci.py
+++ 10870_Fibonacci.py
@@ -6,5 +6,20 @@ def fivonacci(n):
         return 1
     else:
         return fivonacci(n-1) + fivonacci(n-2)
-answer = fivonacci(n)
-print(answer)
+
+def fiv_iter(n):
+    if n == 0: return 0
+    if n == 1: return 1
+
+    pp = 0
+    p = 1
+    for i in range(n-1):
+        answer = pp + p
+        pp = p
+        p = answer
+    return answer
+
+answer1 = fivonacci(n)
+print(answer1)
+answer2 = fiv_iter(n)
+print(answer2)
